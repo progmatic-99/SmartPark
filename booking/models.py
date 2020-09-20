7fb240c6aff0c1b2b4aed463ad573aeab9096d54
Shubham Upreti <shubham.upreti@gmail.com>
2020-09-20 19:35:37 +0530

    Boolean field for parking slot

--- booking/models.py
+++ booking/models.py
@@ -14,7 +14,8 @@ class parking(models.Model):
     """docstring for parking"""
 
     park_loc = models.ForeignKey(prime, on_delete=models.CASCADE)
-    park_no = models.CharField(max_length=100)
+    # True: slot is vacant, False: slot is taken
+    park_no = models.BooleanField(default=True)
     current_status = models.CharField(max_length=100)
 
     def __str__(self):
