9e5c4d311692fe9827cc9c0a144797c2c092ad27
Priyank Rastogi <priyankrastogi14@gmail.com>
2020-09-26 23:42:24 +0530

    Add Google Oauth and firebase (#14)
    * design landing page
    
    * add login page and sign up page
    
    * deleted unnecessary files
    
    * integrate firebase and OAuth
    
    * delete unwanted files

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ import Navbar from './components/layouts/Navbar';
 import LandingPage from './components/container/LandingPage';
 import LoginPage from './components/presentational/all-view/LoginPage/LoginPagePr';
 import RegistrationPage from './components/presentational/all-view/RegistrationPage/RegistrationPagePr';
+// import FireBase from './components/FireBase'
 import './App.css'
+
 function App() {
   return (
     <Router>
@@ -13,6 +15,7 @@ function App() {
         <Route path='/login' exact component={LoginPage} />
         <Route path='/signup' exact component={RegistrationPage} />
         {/* <Route path='/' exact component={LoginPage} /> */}
+        {/* <Route path='/firebase' exact component={FireBase} /> */}
         
       </div>
     </Router>
--- client/src/components/presentational/all-view/LoginPage/LoginPagePr.js
+++ client/src/components/presentational/all-view/LoginPage/LoginPagePr.js
@@ -1,42 +1,108 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import './LoginPagePr.css';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
-const LoginPage = () => {
-    return (
-        <Formik
-            initialValues={{ email: '', password: '' }}
-            validationSchema={Yup.object({
-                password: Yup.string()
-                    .min(4, 'Password is too short - should be 5 chars minimum.')
-                    .required('No password provided.'),
-                email: Yup.string()
-                    .email('Invalid email address')
-                    .required('Required'),
-            })}
-            onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    setSubmitting(false);
-                }, 400);
-            }}
-        >
-            <Form>
+class LoginPage extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            isLogined: false,
+            accessToken: ''
+        };
+        this.login = this.login.bind(this);
+        this.handleLoginFailure = this.handleLoginFailure.bind(this);
+        this.logout = this.logout.bind(this);
+        this.handleLogoutFailure = this.handleLogoutFailure.bind(this);
+    }
+    login(response) {
+        if (response.accessToken) {
+            this.setState(state => ({
+                isLogined: true,
+                accessToken: response.accessToken
+            }));
+        }
+    }
+
+    logout(response) {
+        this.setState(state => ({
+            isLogined: false,
+            accessToken: ''
+        }));
+    }
+
+    handleLoginFailure(response) {
+        alert('Failed to log in')
+    }
+
+    handleLogoutFailure(response) {
+        alert('Failed to log out')
+    }
+    render() {
+        const CLIENT_ID = process.env.REACT_APP_OAUTH_CLIENTID;
+        return (
+            // <div>
+            // <div>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                validationSchema={Yup.object({
+                    password: Yup.string()
+                        .min(4, 'Password is too short - should be 5 chars minimum.')
+                        .required('No password provided.'),
+                    email: Yup.string()
+                        .email('Invalid email address')
+                        .required('Required'),
+                })}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
+                }}
+            >
                 <div>
-                    <label htmlFor="email">Email</label>
-                    <Field name="email" type="email" />
-                    <ErrorMessage name="email" />
+                    <Form>
+                        <div>
+                            <label htmlFor="email">Email</label>
+                            <Field name="email" type="email" />
+                            <ErrorMessage name="email" />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Password</label>
+                            <Field name="password" type="password" />
+                            <ErrorMessage name="password" />
+                        </div>
+                        <button type="submit">Submit</button>
+                    </Form>
+                    <p>OR</p>
+                    <div>
+                        {this.state.isLogined ?
+                            <GoogleLogout
+                                clientId={CLIENT_ID}
+                                buttonText='Logout'
+                                onLogoutSuccess={this.logout}
+                                onFailure={this.handleLogoutFailure}
+                            >
+                            </GoogleLogout> : <GoogleLogin
+                                clientId={CLIENT_ID}
+                                buttonText='Login'
+                                onSuccess={this.login}
+                                onFailure={this.handleLoginFailure}
+                                cookiePolicy={'single_host_origin'}
+                                responseType='code,token'
+                            />
+                        }
+                        {/* {this.state.accessToken ? <h5>Your Access Token: <br /><br /> {this.state.accessToken}</h5> : null} */}
+
+                    </div>
                 </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <Field name="password" type="password" />
-                    <ErrorMessage name="password" />
-                </div>
-                <button type="submit">Submit</button>
-            </Form>
-        </Formik>
-    );
+
+            </Formik>
+
+
+        );
+    }
 };
 
 export default LoginPage;
