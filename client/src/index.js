dd5ca28378f0b64746225f15cdb541559e2499b4
Ashish <as7220004@gmail.com>
2020-09-29 10:53:13 +0530

    npm installation

--- client/src/index.js
+++ client/src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase'
+// import * as firebase from 'firebase'
 
-var firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: "smart-park-290414.firebaseapp.com",
-  databaseURL: "https://smart-park-290414.firebaseio.com",
-  projectId: "smart-park-290414",
-  storageBucket: "smart-park-290414.appspot.com",
-  messagingSenderId: "213018512596",
-  appId: "1:213018512596:web:a6e672484d39eabd4c7f41",
-  measurementId: "G-GMS9Y3X8NR"
-};
+// var firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+//   authDomain: "smart-park-290414.firebaseapp.com",
+//   databaseURL: "https://smart-park-290414.firebaseio.com",
+//   projectId: "smart-park-290414",
+//   storageBucket: "smart-park-290414.appspot.com",
+//   messagingSenderId: "213018512596",
+//   appId: "1:213018512596:web:a6e672484d39eabd4c7f41",
+//   measurementId: "G-GMS9Y3X8NR"
+// };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
